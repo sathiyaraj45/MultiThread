d833e2e2a86321559a0fcccc8e2a94d8d75d2ff8
sathiayaraj45 <sathiyaraj45@gmail.com>
2022-01-30 20:19:07 +0400

    Initial commit

---
+++ src/test/java/com/sathiya/MultiThread/SpringBootMultiThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathiya.MultiThread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
